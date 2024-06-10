06e1ba1fcc36c27bc7b6e748e62319aa0a2e3906
Ati Kumar <145497001+atishayk@users.noreply.github.com>
2024-06-10 14:54:00 -0400

    Updated README and added solution to mergeStringsAlternately.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Leetcode75
+
+This is just a copy of my Leetcode experience! Specifically with the Leetcode 75 problem set :)
---
+++ mergeStringsAlternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = ""
+        min_len = min(len(word1), len(word2))
+        for i in range(min_len):
+            merged += word1[i]
+            merged += word2[i]
+        
+        if len(word1) == min_len:
+            merged += word2[min_len:]
+        else:
+            merged += word1[min_len:]
+
+        return merged
