8006250081dc22fec54cc765271fedfb1bbd7519
Ati Kumar <145497001+atishayk@users.noreply.github.com>
2024-06-10 16:04:15 -0400

    Added solution of GCD of strings.

---
+++ GCDofStrings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # checks if GCD even exists
+        smaller = ""
+        if (str1 + str2 != str2 + str1):
+            return ""
+        else:
+            if len(str1) < len(str2):
+                smaller = str1
+            else:
+                smaller = str2
+            size = gcd(len(str1), len(str2))
+            return smaller[0:size]
+
+
+
+            
+
+
+
