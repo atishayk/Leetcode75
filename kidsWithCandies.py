0fdc32ff944b4d7d8556be71fffdda2256543521
Ati Kumar <145497001+atishayk@users.noreply.github.com>
2024-06-17 22:31:00 -0400

    adding Kids with Candies

---
+++ kidsWithCandies.py
@@ -0,0 +1,21 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max = 0
+        answer = []
+
+        for i in range(len(candies)):
+            if max < candies[i]:
+                max = candies[i]
+
+        for j in range(len(candies)):
+            if candies[j] + extraCandies >= max:
+                answer.append(True) 
+            else:
+                answer.append(False) 
+        
+        return answer
+
+        
+
+
+        
